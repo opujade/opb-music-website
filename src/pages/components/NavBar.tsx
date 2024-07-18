a738af69821b3df2e8f04f315ffb538dc1b66615
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-07-18 19:53:04 +0200

    layout improvements

--- src/pages/components/NavBar.tsx
+++ src/pages/components/NavBar.tsx
@@ -13,11 +13,11 @@ export const NavBar = () => {
 	return (
 		<>
 			<div
-				className={`fixed end-0 flex flex-col h-screen md:justify-between font-century-gothic font-bold uppercase text-white tracking-widest text-end p-2 md:p-7 w-48 ${
+				className={`fixed end-0 flex flex-col h-screen justify-start md:justify-between font-century-gothic font-bold uppercase text-white tracking-widest text-end p-2 md:p-7 w-48 ${
 					isVisible ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"
 				} ease-in-out duration-1000`}
 			>
-				<div className="flex justify-end items-center gap-2">
+				<div className="flex justify-end items-center gap-3 order-2 md:order-1 md:mt-0 mt-8">
 					<a
 						href="https://open.spotify.com/intl-es/artist/2AT7fejvAcAuuF4OIo0cFr?si=hRImq0PfRiuErs7dW1UDPQ"
 						target="_blank"
@@ -50,21 +50,21 @@ export const NavBar = () => {
 						/>
 					</a>
 				</div>
-				<ul>
-					<li className="hover:text-main-yellow hover:drop-shadow-yellow text-xl cursor-pointer duration-300 pb-1">
+				<ul className="order-1 md:order-2">
+					<li className="hover:text-main-yellow hover:drop-shadow-yellow text-xl cursor-pointer duration-300 pb-2">
 						Music
 					</li>
-					<li className="hover:text-main-yellow hover:drop-shadow-yellow text-xl cursor-pointer duration-300 py-1">
+					<li className="hover:text-main-yellow hover:drop-shadow-yellow text-xl cursor-pointer duration-300 py-2">
 						Video
 					</li>
-					<li className="hover:text-main-yellow hover:drop-shadow-yellow text-xl cursor-pointer duration-300 py-1">
+					<li className="hover:text-main-yellow hover:drop-shadow-yellow text-xl cursor-pointer duration-300 py-2">
 						Photo
 					</li>
-					<li className="hover:text-main-yellow hover:drop-shadow-yellow text-xl cursor-pointer duration-300 py-1">
+					<li className="hover:text-main-yellow hover:drop-shadow-yellow text-xl cursor-pointer duration-300 py-2">
 						Tour
 					</li>
 				</ul>
-				<div>
+				<div className="order-3 fixed bottom-0 md:relative md:p-0 p-7">
 					<p className="text-xxs text-gray-300">© 2024 O.P.B.</p>
 					<p className="text-xxs text-gray-300">
 						Developed by{" "}
