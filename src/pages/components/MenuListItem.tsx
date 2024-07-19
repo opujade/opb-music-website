ef60e55901748c376f998c6616c2abff17a3f1a4
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-07-19 12:34:55 +0200

    changed list item for buttons

--- src/pages/components/MenuListItem.tsx
+++ src/pages/components/MenuListItem.tsx
@@ -1,7 +1,7 @@
 export const MenuListItem = ({ option }: { option: string }) => {
 	return (
-		<li className="hover:text-primary hover:-translate-x-10 hover:drop-shadow-yellow text-xl cursor-pointer duration-500 pb-2">
+		<button className="font-century-gothic font-bold uppercase text-white tracking-widest text-end hover:text-primary hover:-translate-x-10 hover:drop-shadow-yellow text-xl cursor-pointer duration-500 pb-2">
 			{option}
-		</li>
+		</button>
 	);
 };
