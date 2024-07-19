695230aeb63d013c4bfd4e66088f93e150428c9b
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-07-19 23:13:32 +0200

    image fix2

--- src/pages/Photo.tsx
+++ src/pages/Photo.tsx
@@ -1,28 +1,28 @@
 const Photo = () => {
 	const images = [
 		{
-			src: "../public/assets/img/photos/photo1.JPG",
+			src: "/assets/img/photos/photo1.JPG",
 		},
 		{
-			src: "../public/assets/img/photos/photo2.JPG",
+			src: "/assets/img/photos/photo2.JPG",
 		},
 		{
-			src: "../public/assets/img/photos/photo3.JPG",
+			src: "/assets/img/photos/photo3.JPG",
 		},
 		{
-			src: "../public/assets/img/photos/photo4.JPG",
+			src: "/assets/img/photos/photo4.JPG",
 		},
 		{
-			src: "../public/assets/img/photos/photo5.JPG",
+			src: "/assets/img/photos/photo5.JPG",
 		},
 		{
-			src: "../public/assets/img/photos/photo6.JPG",
+			src: "/assets/img/photos/photo6.JPG",
 		},
 		{
-			src: "../public/assets/img/photos/photo7.JPG",
+			src: "/assets/img/photos/photo7.JPG",
 		},
 		{
-			src: "../public/assets/img/photos/photo8.JPG",
+			src: "/assets/img/photos/photo8.JPG",
 		},
 	];
 
